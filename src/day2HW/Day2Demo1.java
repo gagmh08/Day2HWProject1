99851c7083e4ecf41a2749b0ae2786c9fa5d7672
owner <owner@DESKTOP-M9RKOA9>
2023-04-09 12:02:16 -0600

    Innitial Commit

---
+++ src/day2HW/Day2Demo1.java
@@ -0,0 +1,16 @@
+package day2HW;
+
+public class Day2Demo1 {
+
+	public void add(int a, int b, int c) {
+		
+		System.out.println(a+b+c);
+		
+	}
+	
+	public void add2(int x, int z) {
+		
+		System.out.println(x+z);
+	}
+	
+}
